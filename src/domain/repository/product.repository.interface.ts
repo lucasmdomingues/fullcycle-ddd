badc6f735999eb75d4c00be5323741c61e7d1485
Lucas M Domingues <lucasmdomingues.dev@gmail.com>
2023-12-29 23:28:13 -0300

    added infrastructure

---
+++ src/domain/repository/product.repository.interface.ts
@@ -0,0 +1,6 @@
+import Product from "../entity/product";
+import RepositoryInterface from "./repository.interface";
+
+interface ProductRepositoryInterface extends RepositoryInterface<Product>{
+    
+}
