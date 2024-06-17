19638f24c707be11e22a54731ed69bd24b138ddb
Lucas M Domingues <lucasmdomingues.dev@gmail.com>
2024-06-17 07:59:38 -0300

    added email svc to send mail event

--- src/domain/product/event/handler/send-email-when-product-is-created-handler.ts
+++ src/domain/product/event/handler/send-email-when-product-is-created-handler.ts
@@ -1,8 +1,15 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import ProductCreatedEvent from "../product-created.event";
 
+export interface EmailService {
+    send(templateName: string, data: any): Promise<void>
+}
+
 export default class SendmailWhenProductIsCreateHandler implements EventHandlerInterface<ProductCreatedEvent> {
+    constructor(private emailService?: EmailService) { }
+
     handle(event: ProductCreatedEvent): void {
+        this.emailService.send("productCreated", event.eventData)
         console.log(`product created event has been called`);
     }
 }
